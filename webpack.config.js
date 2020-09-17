0b82727f2820f797ccaa4d748a1347e9bfe53031
koga <koga@wh-plus.com>
2020-09-17 12:49:43 +0900

    babelでトランスパイル

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,18 @@ module.exports = {
         rules: [
             {test: /\.(png|jpg)$/, use: ['file-loader']},
             {test: /\.css$/, use: ['style-loader', 'css-loader']},
-            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/env"],
+                        plugins: ["transform-class-properties"]
+                    }
+                }
+            }
         ]
     }
 }
